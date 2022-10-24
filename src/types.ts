5094d69ff301ebffa926723debf9aa00663e6b59
Simon <simoonf@vip.qq.com>
2022-10-24 18:23:07 +0800

    feat: Add cancelTextVisible type Whether the control cancelText is visible

--- src/types.ts
+++ src/types.ts
@@ -2,22 +2,23 @@ import type { ReactNode } from 'react';
 import type { StyleProp, TextStyle, ViewStyle } from 'react-native';
 
 export type ConfirmModalContextType = {
-  showConfirmModal: (options: IConfirmModalProps) => void;
+  showConfirmModal: (options: IConfirmModalType) => void;
 };
 
 export interface IConfirmModalProviderProps {
   children: ReactNode;
 }
 
-export interface IConfirmModalProps {
+export type IConfirmModalType = {
   title?: string;
   description?: string;
   cancelText?: string;
-  confirmText: string | undefined;
+  confirmText?: string;
   onCancel?: () => void;
   onConfirm?: () => void;
   cancelStyle?: StyleProp<ViewStyle>;
   confirmStyle?: StyleProp<ViewStyle>;
   cancelTextStyle?: StyleProp<TextStyle>;
   confirmTextStyle?: StyleProp<TextStyle>;
-}
+  cancelTextVisible?: boolean;
+};
