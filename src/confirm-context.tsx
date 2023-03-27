1a3291f2d13df5db27f49d7d984f24a40cfec4a0
Simon <simoonf@vip.qq.com>
2023-03-27 14:54:21 +0800

    feat: Add promise logic

--- src/confirm-context.tsx
+++ src/confirm-context.tsx
@@ -1,32 +1,76 @@
-import React, { createContext, createRef, FC, useContext } from 'react';
-import ConfirmModal from './confirm-modal';
+import React, { createContext, FC, useContext, useState } from 'react';
+import ConfirmDialog from './confirm-dialog';
 import type {
   ConfirmModalContextType,
-  IConfirmModalType,
+  ConfirmModalType,
   IConfirmModalProviderProps,
+  ResolveRejectType,
 } from './types';
 
-const ConfirmModalContext = createContext<ConfirmModalContextType>({
-  showConfirmModal: () => null,
-});
+const ConfirmModalContext = createContext<ConfirmModalContextType>(
+  {} as ConfirmModalContextType
+);
 
-export const useConfirmModal = () => useContext(ConfirmModalContext);
+export const useConfirm = () => useContext(ConfirmModalContext);
+
+const DEFAULT_CONFIRM_OPTION: ConfirmModalType = {
+  cancelText: 'Cancel',
+  confirmText: 'Confirm',
+  cancelTextVisible: true,
+};
 
 export const ConfirmModalProvider: FC<IConfirmModalProviderProps> = ({
   children,
 }) => {
-  const confirmModalRef = createRef<ConfirmModalContextType>();
+  const [isOpen, setIsOpen] = useState(false);
 
-  const showConfirmModal = (options: IConfirmModalType) => {
-    confirmModalRef.current !== null &&
-      confirmModalRef.current.showConfirmModal(options);
+  const [options, setOptions] = useState<ConfirmModalType>(
+    DEFAULT_CONFIRM_OPTION
+  );
+
+  const [resolveReject, setResolveReject] = useState<
+    ResolveRejectType | string[]
+  >([]);
+
+  const [resolve] = resolveReject as ResolveRejectType;
+
+  const confirm = (overrideOptions: ConfirmModalType) =>
+    // eslint-disable-next-line no-shadow
+    new Promise<boolean>((resolve, reject) => {
+      setOptions({ ...DEFAULT_CONFIRM_OPTION, ...overrideOptions });
+
+      setIsOpen(true);
+      setResolveReject([resolve, reject]);
+    });
+
+  const handleCancel = () => {
+    if (resolve) {
+      resolve(false);
+      setIsOpen(false);
+
+      options.onCancel && options.onCancel();
+    }
+  };
+
+  const handleConfirm = () => {
+    if (resolve) {
+      resolve(true);
+      setIsOpen(false);
+
+      options.onConfirm && options.onConfirm();
+    }
   };
 
   return (
-    <ConfirmModalContext.Provider value={{ showConfirmModal }}>
+    <ConfirmModalContext.Provider value={{ confirm }}>
       {children}
 
-      <ConfirmModal ref={confirmModalRef} />
+      <ConfirmDialog
+        {...options}
+        open={isOpen}
+        onCancel={handleCancel}
+        onConfirm={handleConfirm}
+      />
     </ConfirmModalContext.Provider>
   );
 };
